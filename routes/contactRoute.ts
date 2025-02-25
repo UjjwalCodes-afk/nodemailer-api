57de4b58a13d377602a11e5fc59142533f335f75
Ujjwal <ujjwalnagpal3@gmail.com>
2025-02-25 16:26:00 +0530

    rgr

--- routes/contactRoute.ts
+++ routes/contactRoute.ts
@@ -1,9 +1,8 @@
-import express from 'express';
-import { ContactUs } from '../controllers/contactController';
-
+import express from "express";
+import { ContactUs } from "../controllers/contactController";
 
 const contactRoute = express.Router();
 
-contactRoute.post('/contact', ContactUs);
+contactRoute.post("/contact", ContactUs); // Ensure this matches the function signature
 
 export default contactRoute;
